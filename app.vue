5471bb95424c170ebd31ccd9f7a5c7b76092a611
xiaoxiOk11 <xiaoxi112233ok@gmail.com>
2025-11-04 16:37:14 +0800

    update

--- app.vue
+++ app.vue
@@ -10,18 +10,4 @@ import { themeRoute } from '~/stores/theme'
 // 确保在 setup 顶层调用 store
 const useThemeRoute = themeRoute();
 import(`~/assets/css/${useThemeRoute.fileRoute}/main.scss`)
-import icon from '~/assets/images/i1.png'
-useHead({
-  title: 'Nuxt 3 Demo',
-
-  meta: [
-    {
-      name: 'description',
-      content: 'This is a demo Nuxt 3 application.'
-    }
-  ],
-  link: [
-    { rel: 'icon', type: 'image/x-icon', href: icon }
-  ]
-})
 </script>
